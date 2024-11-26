f2e08e5ececfad33a111b7f68ba8f20df472c451
Alvaro Marenco <alvaromarencom@gmail.com>
2024-11-25 23:17:01 -0600

    Configuracion de MUI con Vite

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,8 +1,14 @@
+import { MailOutline } from "@mui/icons-material"
+import { Typography } from "@mui/material"
 
 export const JournalPage = () => {
   return (
-    <div>
-      Journal Page
-    </div>
+    <>
+        <Typography variant="h1">
+          Journal Page
+        </Typography>
+        <MailOutline />
+    </>
+    
   )
 }
