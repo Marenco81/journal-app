0a2807a087190afa4398235b775281cbf4eefb78
Alvaro Marenco <alvaromarencom@gmail.com>
2024-11-27 15:03:40 -0600

    JournalLayout y JournalPage

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,14 +1,14 @@
 import { MailOutline } from "@mui/icons-material"
 import { Typography } from "@mui/material"
+import { JournalLayout } from "../layout/JournalLayout"
 
 export const JournalPage = () => {
   return (
-    <>
-        <Typography variant="h1">
-          Journal Page
+    <JournalLayout>
+        <Typography>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Incidunt nesciunt minima enim unde quae atque corrupti. Ipsam sunt nostrum, quidem quas quisquam quasi minima dolore voluptatibus, ut vel omnis provident?
         </Typography>
-        <MailOutline />
-    </>
+    </JournalLayout>
     
   )
 }
