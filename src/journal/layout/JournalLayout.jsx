dbcb57a8c32b47aead83afbcd997a516fd6deb64
Alvaro Marenco <alvaromarencom@gmail.com>
2024-11-27 16:35:14 -0600

    SideBar - Diseño

--- src/journal/layout/JournalLayout.jsx
+++ src/journal/layout/JournalLayout.jsx
@@ -1,8 +1,8 @@
-import { Box } from "@mui/material";
-import { NavBar } from "../components";
+import { Box, Toolbar } from "@mui/material";
+import { NavBar, SideBar } from "../components";
 
 
-const drawerWidth = 240;
+const drawerWidth = 280;
 
 
 export const JournalLayout = ({children}) => {
@@ -12,13 +12,13 @@ export const JournalLayout = ({children}) => {
 
         <NavBar drawerWidth={drawerWidth}/>
 
-        {/* Sidebar */}
+        <SideBar drawerWidth={drawerWidth}/>
 
         <Box 
             component="main"
             sx={{flexGrow:1, p:3}}
         >
-            {/* Toolbar */}
+            <Toolbar/>
             {children}
 
         </Box>
