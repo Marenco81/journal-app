4938c6b18c5a9d7011ce9551c5d76a4e0380bf5d
Alvaro Marenco <alvaromarencom@gmail.com>
2024-11-27 17:21:57 -0600

    NothingSelectedView

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,13 +1,14 @@
 import { MailOutline } from "@mui/icons-material"
 import { Typography } from "@mui/material"
 import { JournalLayout } from "../layout/JournalLayout"
+import { NothingSelectedView } from "../views"
 
 export const JournalPage = () => {
   return (
     <JournalLayout>
-        <Typography>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Incidunt nesciunt minima enim unde quae atque corrupti. Ipsam sunt nostrum, quidem quas quisquam quasi minima dolore voluptatibus, ut vel omnis provident?
-        </Typography>
+        <NothingSelectedView/>
+
+        {/* NoteView */}
     </JournalLayout>
     
   )
