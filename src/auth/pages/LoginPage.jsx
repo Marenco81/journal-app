9c2ec26223dd1fffe20273970d93e68d2f3d7112
Alvaro Marenco <alvaromarencom@gmail.com>
2024-12-19 17:41:43 -0600

    Animaciones para la aplicación

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -36,7 +36,8 @@ export const LoginPage = () => {
   return (
     <AuthLayout title="Login">
 
-        <form onSubmit={onSubmit}>
+        <form onSubmit={onSubmit} className="animate__animated animate__fadeIn animate__fast">
+      
 
           <Grid container spacing={2} sx={{mb:2, mt:1}}>
             <Grid item xs={12} sx={{mt:2}}>
