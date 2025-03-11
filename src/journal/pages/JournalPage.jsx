4dd845a0c54aaa68928f6ebf926c1e342c667473
Alvaro Marenco <alvaromarencom@gmail.com>
2025-03-11 13:30:38 -0600

    Active note functionality

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -3,24 +3,29 @@ import { IconButton } from "@mui/material"
 import { JournalLayout } from "../layout/JournalLayout"
 import { NoteView, NothingSelectedView } from "../views"
 import { AddOutlined } from "@mui/icons-material"
-import { useDispatch } from "react-redux"
+import { useDispatch, useSelector } from "react-redux"
 import { startNewNote } from "../../store/journal"
 
 export const JournalPage = () => {
 
   const dispatch = useDispatch();
 
+  const {isSaving, active} = useSelector(state => state.journal )
+
   const onClickNewNote = () => {
     dispatch (startNewNote ());
   }
   return (
     <JournalLayout>
-        <NothingSelectedView/>
+
+        {(active)?  <NoteView/> : <NothingSelectedView/> }
+        {/* <NothingSelectedView/> */}
 
         {/* <NoteView/> */}
 
         <IconButton
           onClick={onClickNewNote}
+          disabled={isSaving}
           size="large"
           sx={{
             color:'white',
--- src/store/journal/thunks.js
+++ src/store/journal/thunks.js
@@ -1,8 +1,11 @@
 import { collection, doc, setDoc } from "firebase/firestore/lite";
 import { FirebaseDB } from "../../firebase/config";
+import { addNewEmptyNote, savingNewNote, setActiveNote } from "./journalSlice";
 
 export const startNewNote = () => {
     return async (dispatch, getState) => {
+
+        dispatch (savingNewNote() );
 
         const {uid} = getState().auth;
         //uid
@@ -15,10 +18,12 @@ export const startNewNote = () => {
         const newDoc = doc(collection(FirebaseDB, `${uid}/journal/notes`));
         const setDocResp = await setDoc(newDoc, newNote);
 
-        console.log({newDoc, setDocResp});
+        newNote.id = newDoc.id;
+
+        // console.log({newDoc, setDocResp});
 
         //! dispatch
-        // dispatch (newNote)
-        // dispatch (activeNote)
+        dispatch (addNewEmptyNote (newNote));
+        dispatch (setActiveNote (newNote));
     }
 }
