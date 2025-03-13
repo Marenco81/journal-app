2c51bb9e565f67365ba3f4119fc4ea9af4224432
Alvaro Marenco <alvaromarencom@gmail.com>
2025-03-12 18:48:30 -0600

    Setting a note as Active

--- src/journal/components/SideBarItem.jsx
+++ src/journal/components/SideBarItem.jsx
@@ -1,15 +1,23 @@
 import { TurnedInNot } from "@mui/icons-material"
 import { Grid2, ListItem, ListItemButton, ListItemIcon, ListItemText } from "@mui/material"
 import { useMemo } from "react";
+import { setActiveNote } from "../../store/journal";
+import { useDispatch } from "react-redux";
 
-const SideBarItem = ({title = '', body, id}) => {
+const SideBarItem = ({title = '', body, id, date, imageUrls = []}) => {
+
+    const dispatch = useDispatch();
+
+    const onClickNote = () => {
+        dispatch (setActiveNote({title, body, id, date, imageUrls}))
+    }
 
     const newTitle = useMemo ( () => {
         return title.length > 17 ? title.substring (0, 17) + '...' : title;
     }, [title]);
   return (
     <ListItem  disablePadding>
-        <ListItemButton>
+        <ListItemButton onClick={onClickNote}>
             <ListItemIcon>
                 <TurnedInNot/>
             </ListItemIcon>
