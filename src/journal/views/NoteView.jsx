1402391addc5f85ce51a438ee52bb94ce3603a4e
Alvaro Marenco <alvaromarencom@gmail.com>
2025-03-17 16:21:07 -0600

    Selecting files to upload from React

--- src/journal/views/NoteView.jsx
+++ src/journal/views/NoteView.jsx
@@ -1,8 +1,8 @@
-import { SaveOutlined } from "@mui/icons-material"
-import { Button, Grid, TextField, Typography } from "@mui/material"
+import { SaveOutlined, UploadOutlined } from "@mui/icons-material"
+import { Button, Grid, IconButton, TextField, Typography } from "@mui/material"
 import { ImageGallery } from "../components"
 import { useForm } from "../../hooks"
-import { useEffect, useMemo } from "react"
+import { useEffect, useMemo, useRef } from "react"
 import { useDispatch, useSelector } from "react-redux"
 import { setActiveNote, startSaveNote } from "../../store/journal"
 import Swal from "sweetalert2"
@@ -22,6 +22,8 @@ export const NoteView = () => {
         return newDate.toUTCString();
      }, [date]); 
 
+     const fileInputRef = useRef();
+
      useEffect(() => {
         dispatch(setActiveNote(formState));
      }, [formState]);
@@ -34,6 +36,13 @@ export const NoteView = () => {
 
      const onSaveNote = () => {
         dispatch(startSaveNote());
+     }
+
+     const onFileInputChange = ({target }) => {
+        if(target.files === 0) return;
+
+        console.log('subiendo archivos');
+        // dispatch(startUploadingFiles(target.files)); 
      }
     
   return (
@@ -50,6 +59,25 @@ export const NoteView = () => {
             </Typography>
         </Grid>
         <Grid item>
+            
+            <input
+                ref={fileInputRef}
+                type="file"
+                multiple
+                onChange={onFileInputChange}
+                style={{display:'none'}}
+            />
+
+            <IconButton 
+                color="primary"
+                disabled={isSaving}
+                onClick={() => fileInputRef.current.click()}
+                sx={{fontSize:20}}
+            >
+                
+                <UploadOutlined sx={{fontSize:25, mr:1}}/>
+                Imágenes
+            </IconButton>
             <Button
                 disabled={isSaving}
                 onClick={onSaveNote} 
